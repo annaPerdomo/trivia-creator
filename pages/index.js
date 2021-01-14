2a05f01141010201069ca2c33791d5a471896977
Anna Miura <annamaliamiura@gmail.com>
2021-01-14 14:16:28 -0800

    feat: change home page

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,103 @@
+import React, {useState, useEffect} from 'react';
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+
 export default function Home() {
+  const title = "Trivia Creator | Create trivia questions & answers and then play with a group | Trivia";
+const desc =
+'Trivia creator allows you to host trivia nights with your friends!';
+//Module parse failed: Unexpected character '�' (1:0)
+//You may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders
+//getting error above trying to change this
+// const image = require(`../public/testBrain.jpg`);
+const url = 'www.notsure.help';
+const keywords = 'trivia';
+const robots = 'index, follow';
+
   return (
-    <div className={styles.container}>
+    <React.Fragment>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{title}</title>
+        <meta content={desc} name="description" />
+        <meta content={keywords} name="keywords" />
+        <meta content={robots} name="robots" />
+{/*
+        <meta content={title} property="og:title" />
+        <meta content="website" property="og:type" />
+        <meta content={url} property="og:url" />
+        <meta content={desc} property="og:description" />
+        <meta content={image} property="og:image" />
+        <meta content="summary_large_image" name="twitter:card" />
+        <meta content="getjobfit.com" name="twitter:domain" />
+        <meta content={title} name="twitter:title" />
+        <meta content={desc} name="twitter:description" />
+        <meta content={image} name="twitter:image" />
+        <meta content={url} name="twitter:url" /> */}
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      <div>
+        <h1>HEY GUYS</h1>
+      </div>
+    </React.Fragment>
   )
+  // return (
+  //   <div className={styles.container}>
+  //     <Head>
+  //       <title>Create Next App</title>
+  //       <link rel="icon" href="/favicon.ico" />
+  //     </Head>
+
+  //     <main className={styles.main}>
+  //       <h1 className={styles.title}>
+  //         Welcome to <a href="https://nextjs.org">Next.js!</a>
+  //       </h1>
+
+  //       <p className={styles.description}>
+  //         Get started by editing{' '}
+  //         <code className={styles.code}>pages/index.js</code>
+  //       </p>
+
+  //       <div className={styles.grid}>
+  //         <a href="https://nextjs.org/docs" className={styles.card}>
+  //           <h3>Documentation &rarr;</h3>
+  //           <p>Find in-depth information about Next.js features and API.</p>
+  //         </a>
+
+  //         <a href="https://nextjs.org/learn" className={styles.card}>
+  //           <h3>Learn &rarr;</h3>
+  //           <p>Learn about Next.js in an interactive course with quizzes!</p>
+  //         </a>
+
+  //         <a
+  //           href="https://github.com/vercel/next.js/tree/master/examples"
+  //           className={styles.card}
+  //         >
+  //           <h3>Examples &rarr;</h3>
+  //           <p>Discover and deploy boilerplate example Next.js projects.</p>
+  //         </a>
+
+  //         <a
+  //           href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+  //           className={styles.card}
+  //         >
+  //           <h3>Deploy &rarr;</h3>
+  //           <p>
+  //             Instantly deploy your Next.js site to a public URL with Vercel.
+  //           </p>
+  //         </a>
+  //       </div>
+  //     </main>
+
+  //     <footer className={styles.footer}>
+  //       <a
+  //         href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Powered by{' '}
+  //         <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+  //       </a>
+  //     </footer>
+  //   </div>
+  // )
 }
