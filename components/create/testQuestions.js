8c15d8db9c03dfcae2fb9659ab5cecb9495ab33d
Corrie Grimshaw <corrie.e.grimshaw@gmail.com>
2021-04-22 19:59:08 -0700

    Feature: Refactor css on /create page. Add functionality for click through questions.

---
+++ components/create/testQuestions.js
@@ -0,0 +1,7 @@
+const q = [];
+
+for (var i=0; i < 10; i++){
+   q.push("Purus in mollis nunc sed id semper risus in. Sapien nec sagittis aliquam malesuada bibendum. Cras sed felis eget velit aliquet sagittis id.");
+}
+
+export default q;
