a374cdfd378192746b80b65436fdf3e6d46c7a1c
Anna Miura <annamaliamiura@gmail.com>
2021-05-21 15:18:17 -0700

    feat: set triviaId, display round number on play page and add route to round overview button

--- components/PlayGame/PlayGame.jsx
+++ components/PlayGame/PlayGame.jsx
@@ -1,11 +1,21 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux';
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 
 export default function PlayGame({questions}) {
+  const [triviaId, setTriviaId] = useState(null);
+  useEffect(() => {
+    if (questions) {
+      setTriviaId(questions[0].triviaId)
+    }
+  }, [])
+  console.log({questions});
+  const router = useRouter();
+  const roundNum = router.query.round.split('-')[1];
   return (
     <div>
-      <h1>Why hello there</h1>
+      <h1>Round {roundNum}</h1>
       <div>
         <ul>
           {questions.map(question => {
@@ -15,6 +25,9 @@ export default function PlayGame({questions}) {
             })}
         </ul>
       </div>
+      <Link href={`/game/${triviaId}/round-${roundNum}/overview`}>
+        <button onClick={(e) => console.log('this is also being fired wooo')}>Submit</button>
+      </Link>
     </div>
   )
 }
