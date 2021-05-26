f11d6bb48ce4fd6899168a36295b83d0b5be083a
Anna Miura <annamaliamiura@gmail.com>
2021-05-26 12:44:02 -0700

    feat: add auth session state to _app.js based on nextAuth add session state document

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,18 @@
 import '../styles/globals.css'
 import { wrapper } from "../redux/store"
 import {useStore} from '../redux/store';
-import { Provider } from 'react-redux'
+import { Provider as ReduxProvider}  from 'react-redux';
+import { Provider as NextAuthProvider} from 'next-auth/client'
 
 
 export default function App({ Component, pageProps }) {
   const store = useStore(pageProps.initialReduxState)
 
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <NextAuthProvider session={pageProps.session}>
+      <ReduxProvider store={store}>
+        <Component {...pageProps} />
+      </ReduxProvider>
+    </NextAuthProvider>
   )
 }
