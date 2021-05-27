f61f2ba360074a7830437ffa3138641e9d64ad0d
Anna Miura <annamaliamiura@gmail.com>
2021-05-27 14:44:39 -0700

    refactor: Change index page to display signIn button if user is not authenticated

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import { signIn, useSession } from "next-auth/client";
+import { signin, signIn, signOut, useSession } from "next-auth/client";
 import Link from 'next/link';
 import styles from "../styles/Home.module.css";
 const {
@@ -9,14 +9,19 @@ const {
   divider,
   homePageButtons,
   welcomeBanner,
+  signInButtonContainer,
+  signInButton
 } = styles;
 
 export default function Home() {
-  const [session] = useSession();
+  const [session, loading] = useSession();
   useEffect(() => {
     if (session?.error === "RefreshAccessTokenError") {
       console.log('❕❗️❕❗️RefreshAccessTokenError!!', session);
       signIn(); // Force sign in to hopefully resolve error
+    }
+    if (session) {
+      console.log({session});
     }
   }, [session]);
   return (
@@ -24,17 +29,23 @@ export default function Home() {
       <div className={welcomeBanner}>
         <h1>Welcome to Trivia DeathMatch&trade;</h1>
       </div>
-      <div className={buttonContainer}>
-        <div className={buttonSection}>
-          <Link href="/create">
-            <button className={homePageButtons}>Create A Game</button>
-          </Link>
+      {!session ? (
+        <div className={`${buttonContainer}, ${signInButtonContainer}`}>
+          <button onClick={() => signIn()}>Sign in</button>
         </div>
-        <div className={divider}></div>
-        <div className={buttonSection}>
-          <button className={homePageButtons}>Start A Game</button>
+      ) : (
+        <div className={buttonContainer}>
+          <div className={buttonSection}>
+            <Link href="/create">
+              <button className={homePageButtons}>Create A Game</button>
+            </Link>
+          </div>
+          <div className={divider}></div>
+          <div className={buttonSection}>
+            <button className={homePageButtons}>Start A Game</button>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,16 @@
 import React, {useState, useEffect} from 'react';
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import Home from '../components/Home';
-import Header from '../components/Header/Header';
 
 
 export default function HomePage() {
-  const title = "Trivia Creator | Create trivia questions & answers and then play with a group | Trivia";
-const desc =
-'Trivia creator allows you to host trivia nights with your friends!';
-//You may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders
-//getting error above trying to change this
-// const image = require(`../public/testBrain.jpg`);
-const url = 'www.notsure.help';
-const keywords = 'trivia';
-const robots = 'index, follow';
+  const title =
+    'Trivia Creator | Create trivia questions & answers and then play with a group | Trivia';
+  const desc =
+    'Trivia creator allows you to host trivia nights with your friends!';
+  const url = 'www.notsure.help';
+  const keywords = 'trivia';
+  const robots = 'index, follow';
 
   return (
     <React.Fragment>
@@ -23,7 +19,7 @@ const robots = 'index, follow';
         <meta content={desc} name="description" />
         <meta content={keywords} name="keywords" />
         <meta content={robots} name="robots" />
-{/*
+        {/*
         <meta content={title} property="og:title" />
         <meta content="website" property="og:type" />
         <meta content={url} property="og:url" />
@@ -36,68 +32,7 @@ const robots = 'index, follow';
         <meta content={image} name="twitter:image" />
         <meta content={url} name="twitter:url" /> */}
       </Head>
-      <Header />
       <Home />
     </React.Fragment>
-  )
-  // return (
-  //   <div className={styles.container}>
-  //     <Head>
-  //       <title>Create Next App</title>
-  //       <link rel="icon" href="/favicon.ico" />
-  //     </Head>
-
-  //     <main className={styles.main}>
-  //       <h1 className={styles.title}>
-  //         Welcome to <a href="https://nextjs.org">Next.js!</a>
-  //       </h1>
-
-  //       <p className={styles.description}>
-  //         Get started by editing{' '}
-  //         <code className={styles.code}>pages/index.js</code>
-  //       </p>
-
-  //       <div className={styles.grid}>
-  //         <a href="https://nextjs.org/docs" className={styles.card}>
-  //           <h3>Documentation &rarr;</h3>
-  //           <p>Find in-depth information about Next.js features and API.</p>
-  //         </a>
-
-  //         <a href="https://nextjs.org/learn" className={styles.card}>
-  //           <h3>Learn &rarr;</h3>
-  //           <p>Learn about Next.js in an interactive course with quizzes!</p>
-  //         </a>
-
-  //         <a
-  //           href="https://github.com/vercel/next.js/tree/master/examples"
-  //           className={styles.card}
-  //         >
-  //           <h3>Examples &rarr;</h3>
-  //           <p>Discover and deploy boilerplate example Next.js projects.</p>
-  //         </a>
-
-  //         <a
-  //           href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-  //           className={styles.card}
-  //         >
-  //           <h3>Deploy &rarr;</h3>
-  //           <p>
-  //             Instantly deploy your Next.js site to a public URL with Vercel.
-  //           </p>
-  //         </a>
-  //       </div>
-  //     </main>
-
-  //     <footer className={styles.footer}>
-  //       <a
-  //         href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Powered by{' '}
-  //         <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-  //       </a>
-  //     </footer>
-  //   </div>
-  // )
+  );
 }
