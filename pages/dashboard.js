5ba1c699f5a1780dc7ca3d725df7de269fcf1c4c
Anna Miura <annamaliamiura@gmail.com>
2021-06-01 15:28:48 -0700

    feat: redirect user to home page if trying to visit dashboard unauthenticated

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,9 +2,12 @@ import React, {useState, useEffect} from 'react';
 import { useSession, getSession } from 'next-auth/client'
 import Head from 'next/head'
 import Dashboard from '../components/Dashboard/Dashboard';
+import { useRouter } from 'next/router';
 
 
 export default function DashboardPage() {
+  const [ session, loading ] = useSession();
+  const router = useRouter();
   const title =
     'Trivia Creator | Create trivia questions & answers and then play with a group | Trivia';
   const desc =
@@ -12,28 +15,45 @@ export default function DashboardPage() {
   const url = 'www.notsure.help';
   const keywords = 'trivia';
   const robots = 'index, follow';
-  return (
-    <React.Fragment>
-      <Head>
-        <title>{title}</title>
-        <meta content={desc} name="description" />
-        <meta content={keywords} name="keywords" />
-        <meta content={robots} name="robots" />
-        {/*
-        <meta content={title} property="og:title" />
-        <meta content="website" property="og:type" />
-        <meta content={url} property="og:url" />
-        <meta content={desc} property="og:description" />
-        <meta content={image} property="og:image" />
-        <meta content="summary_large_image" name="twitter:card" />
-        <meta content="getjobfit.com" name="twitter:domain" />
-        <meta content={title} name="twitter:title" />
-        <meta content={desc} name="twitter:description" />
-        <meta content={image} name="twitter:image" />
-        <meta content={url} name="twitter:url" /> */}
-      </Head>
-      <Dashboard />
-    </React.Fragment>
-  );
+
+  const pageIsLoadedOnClient = typeof window !== 'undefined';
+  const userIsLoggedIn = session ? true : false;
+
+  if (pageIsLoadedOnClient) {
+    if (userIsLoggedIn) {
+      return (
+        <React.Fragment>
+          <Head>
+            <title>{title}</title>
+            <meta content={desc} name="description" />
+            <meta content={keywords} name="keywords" />
+            <meta content={robots} name="robots" />
+            {/*
+            <meta content={title} property="og:title" />
+            <meta content="website" property="og:type" />
+            <meta content={url} property="og:url" />
+            <meta content={desc} property="og:description" />
+            <meta content={image} property="og:image" />
+            <meta content="summary_large_image" name="twitter:card" />
+            <meta content="getjobfit.com" name="twitter:domain" />
+            <meta content={title} name="twitter:title" />
+            <meta content={desc} name="twitter:description" />
+            <meta content={image} name="twitter:image" />
+            <meta content={url} name="twitter:url" /> */}
+          </Head>
+          <Dashboard />
+        </React.Fragment>
+      );
+    } else {
+      router.push('/')
+    }
+  }
+  return null;
 }
 
+export async function getServerSideProps(context) {
+  const session = await getSession(context)
+  return {
+    props: { session }
+  }
+}
