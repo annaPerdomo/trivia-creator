93ec1cf6c3fb6ea07368a87a389377f934aec35b
Anna Miura <annamaliamiura@gmail.com>
2021-07-09 16:26:55 -0700

    feat: handle user not logged in but tryin gto access play round edge case

---
+++ pages/game/[joinCode]/[round]/overview.jsx
@@ -0,0 +1,98 @@
+import * as React from 'react'
+// import type { GetServerSideProps, NextPage } from 'next'
+import { signIn, getSession } from 'next-auth/client'
+import { useRouter } from 'next/router'
+import Head from 'next/head'
+import RoundOverview from '../../../../src/components/RoundOverview/RoundOverview'
+import prisma from '../../../../lib/prisma'
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  const userIsNotLoggedIn = !session?.user
+  if (userIsNotLoggedIn) {
+    return {
+      props: {
+        session: null, 
+        questions: []
+      }
+    }
+  } else {
+    const {joinCode, round} = context.params;
+    const roundNum = Number(round.slice(round.length - 1));
+    const triviaGame = await prisma.triviaGame.findUnique({
+      where: {
+        joinCode
+      }
+    })
+    const currentGameId = triviaGame.id;
+    const fetchedQuestions = await prisma.question.findMany({
+      where: {
+        triviaId: currentGameId,
+        roundNum
+      },
+      include: {
+        answers: true
+      }
+    });
+    if (fetchedQuestions) {
+      const questions = fetchedQuestions.map(question => {
+        if (question?.answers?.submittedAt) {
+          return {
+            ...question, 
+            submittedAt: question.answers.submittedAt.toString()
+          }
+        } else {
+          return {
+            ...question
+          }
+        }
+      })
+      return {
+        props: { session, questions },
+      }
+    } else {
+      return {
+        props: { session, questions: [] }
+      }
+    }
+  }
+}
+
+
+
+
+export default function RoundOverviewPage(props) {
+  const title =
+    'Trivia Creator | Create trivia questions & answers and then play with a group | Trivia';
+  const desc =
+    'Trivia creator allows you to host trivia nights with your friends!';
+  const keywords = 'trivia';
+  const robots = 'index, follow';
+
+  const pageIsLoadedOnClient = typeof window !== 'undefined';
+  const userIsLoggedIn = props.session ? true : false;
+  console.log({props})
+  if (pageIsLoadedOnClient) {
+    if (userIsLoggedIn) {
+      return (
+        <React.Fragment>
+          <Head>
+            <title>{title}</title>
+            <meta content={desc} name="description" />
+            <meta content={keywords} name="keywords" />
+            <meta content={robots} name="robots" />
+          </Head>
+          <RoundOverview {...props} />
+        </React.Fragment>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={() => signIn()}>Sign in</button>
+        </div>
+      )
+    }
+  }
+  return null;
+}
+
