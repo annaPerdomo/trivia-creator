d00b26c569614d3a6ef00574bf25e4ef5da09567
Anna Miura <annamaliamiura@gmail.com>
2021-07-10 08:26:34 -0700

    refactor: add helpers to serverSideFetching on round play route

--- pages/game/[joinCode]/[round]/play.jsx
+++ pages/game/[joinCode]/[round]/play.jsx
@@ -1,57 +1,31 @@
 import * as React from 'react'
 import Head from 'next/head'
-import { signIn, getSession } from 'next-auth/client'
+import { signIn } from 'next-auth/client'
 import PlayGame from '../../../../src/components/PlayGame/PlayGame'
 import prisma from '../../../../lib/prisma'
+import {
+  getQuestionsForCurrentRound,
+  getTriviaIdFromJoinCode,
+  userSessionIfLoggedIn
+} from '../../../../lib/helperFunctions/Prisma/runOnServer'
+
 
 export async function getServerSideProps(context) {
-  const session = await getSession(context)
-  const userIsNotLoggedIn = !session?.user
-  if (userIsNotLoggedIn) {
-    return {
-      props: {
-        session: null, 
-        questions: []
-      }
-    }
-  } else {
-    const {joinCode, round} = context.params;
-    const roundNum = Number(round.slice(round.length - 1));
-    const triviaGame = await prisma.triviaGame.findUnique({
-      where: {
-        joinCode
-      }
-    })
-    const currentGameId = triviaGame.id;
-    const fetchedQuestions = await prisma.question.findMany({
-      where: {
-        triviaId: currentGameId,
-        roundNum
-      },
-    });
-    if (fetchedQuestions) {
-      const questions = fetchedQuestions.map(question => {
-        if (question?.answers?.submittedAt) {
-          return {
-            ...question, 
-            submittedAt: question.answers.submittedAt.toString()
-          }
-        } else {
-          return {
-            ...question
-          }
-        }
-      })
-      return {
-        props: { session, questions },
-      }
-    } else {
-      return {
-        props: { session, questions: [] }
-      }
+  const {joinCode, round} = context.params;
+
+  const session = await userSessionIfLoggedIn(context)
+
+  const triviaGameId = await getTriviaIdFromJoinCode(joinCode, prisma);
+  const roundNum = Number(round.slice(round.length - 1));
+
+  const questions = await getQuestionsForCurrentRound(triviaGameId, roundNum, prisma);
+  return {
+    props: {
+      session, questions
     }
   }
 }
+
 
 export default function PlayRound(props) {
   const title =
@@ -86,5 +60,6 @@ export default function PlayRound(props) {
       )
     }
   }
+  return null;
 }
 
