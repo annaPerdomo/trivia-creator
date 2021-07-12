86895448a21007b9e0c48966899db5d58edc3584
Anna Miura <annamaliamiura@gmail.com>
2021-07-11 17:07:23 -0700

    feat: create api call to fetch teams and users for the trivia game

---
+++ pages/api/get/teams.js
@@ -0,0 +1,43 @@
+import prisma from '../../../lib/prisma.ts';
+import Cors from 'cors';
+
+const cors = Cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'HEAD'],
+})
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+export default async function handle(req, res) {
+  try {
+    const { triviaId } = req.body
+    await runMiddleware(req, res, cors);
+    const currentGamesTeams = await prisma.triviaGame.findUnique({
+      where: {
+        id: triviaId
+      },
+      include: {
+        teams: {
+          select: {
+            members: true
+          }
+        }
+      }
+    })
+    res.send(currentGamesTeams)
+  } catch (err) {
+    if (err) {
+      res.send(err)
+    }
+  }
+}
