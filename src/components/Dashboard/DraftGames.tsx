eff9d50789e6896687ca7ad623ed80546fc72f18
Anna Miura <annamaliamiura@gmail.com>
2022-01-20 21:39:35 -0800

    refactor: Convert DraftGames to TypeScript

---
+++ src/components/Dashboard/DraftGames.tsx
@@ -0,0 +1,40 @@
+// @ts-check
+import * as React from 'react'
+import Link from 'next/link';
+import styles from "../../styles/DraftGames.module.css";
+
+const {
+  draftGamesBody,
+  draftGamesContainer,
+  draftGamesFooter,
+  draftGamesHeader
+} = styles;
+
+interface Props {
+  draftGames: any;
+}
+
+const DraftGames: React.FC <Props> = ({ draftGames }) => {
+  return (
+    <div className={draftGamesContainer}>
+      <div className={draftGamesHeader}>
+        <h3>Draft Games</h3>
+      </div>
+
+      <div className={draftGamesBody}>
+        {draftGames.map((draftGame) => (
+          <Link href={`/create/${draftGame.joinCode}`} key={draftGame.id}>
+            <button>Game started {draftGame.createdAt}</button>
+          </Link>
+        ))}
+      </div>
+
+      <div className={draftGamesFooter}>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default DraftGames
